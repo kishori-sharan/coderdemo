766e3813ed342cc171030a20519e8b57971aa091
admin <admin@my-java-workspace>
2022-07-25 15:21:43 +0000

    Added another message

--- com/mxs/HelloCoder.java
+++ com/mxs/HelloCoder.java
@@ -3,5 +3,6 @@ package com.mxs;
 public class HelloCoder {
     public static void main(String[] args) {
         System.out.println("Hello Coder!");
+        System.out.println("Today is " + java.time.LocalDate.now());
     }
 }
